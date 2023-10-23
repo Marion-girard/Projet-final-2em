95ae3eb8b4e16addfd239af0c23c7c10d1cf1391
Marion-girard <marion.girard44@gmail.com>
2023-10-23 22:32:14 +0200

    travail fini

--- api.js
+++ api.js
@@ -2,6 +2,9 @@
 
 class LoadPicture{
     constructor(params){
+        /**
+         * @var {String} url url de l'api
+         */
         this.url = params.url;
         this.area = params.area ?? '.photo';
 
@@ -9,10 +12,12 @@ class LoadPicture{
          * @var {Element} areaDom zone du DOM où générer le slider : Objet du DOM
          */
         this.areaDom = null;
-
+/**
+         * @var {Number} url nombre des photo
+         */
         this.numberPhoto = params.numberPhoto ?? 10
 
-        this.loadingImage = null;
+       // this.loadingImage = null;
         this.createInterface()
         
         this.getUrl()
@@ -22,13 +27,15 @@ class LoadPicture{
     }
 
     createInterface(){
-
+        //on récupère la zone préfaite
             this.areaDom = document.querySelector(this.area);
             if (!this.areaDom) throw new Error(`SLIDER : la zone du DOM "${this.area}" n'existe pas dans le document HTML`);
         
+            //on crée un div pour contenire 
             this.container = document.createElement('div');
             this.areaDom.appendChild(this.container);
         
+             //on crée la div image  pour l'image sur chargement
             this.loadingImage = document.createElement('img');
             this.loadingImage.src = 'imageLoad';
             this.loadingImage.alt = 'Loading...';
@@ -38,25 +45,29 @@ class LoadPicture{
             
         }
         
-        showLoadingImage() {
+        /*showLoadingImage() {
             this.loadingImage.style.display = 'block';
         }
     
         hideLoadingImage() {
             this.loadingImage.style.display = 'none';
-        }
-        async getUrl(){
+        }*/
+        async getUrl(){ //récupère et lie l'url à la div image
             try{
                 for (let i =0; i<this.numberPhoto ; i++){
                     const id = i
                     let responseImg = await fetch (`https://jsonplaceholder.typicode.com/photos?id=${id}`)
                     let imgUrl =  await responseImg.json()
                     let url = imgUrl[0]?.url;
+                    //création de la div image
                     const imageElement = document.createElement('img');
                     imageElement.src = url;
+                    imageElement.id = "load"
                     this.container.appendChild(imageElement);
-                    this.hideLoadingImage();
+                  //  this.hideLoadingImage();
                 }
+
+                //création du bouton
                 this.buttonContainer = document.createElement('div');
                 this.areaDom.appendChild(this.buttonContainer);
             
@@ -67,11 +78,11 @@ class LoadPicture{
             }
             catch(e){
                 console.error(`Une erreur s'est produite : ${e.message}`)
-                this.hideLoadingImage();
+              //  this.hideLoadingImage();
             }
         }
 
-        async getUrl10(){
+        async getUrl10(){ //fonction qui se met en route avec le bouton
             try{
                 for (let i =this.numberPhoto; i<(10+this.numberPhoto) ; i++){
                     const id = i
@@ -80,52 +91,20 @@ class LoadPicture{
                     let url = imgUrl[0]?.url;
                     const imageElement = document.createElement('img');
                     imageElement.src = url;
+                    imageElement.id = "load"
                     this.container.appendChild(imageElement);
-                    this.hideLoadingImage();
-
+                  //  this.hideLoadingImage();
+                   
 
                 }
-                this.buttonContainer = document.createElement('div');
-                this.areaDom.appendChild(this.buttonContainer);
-            
-                this.suivant = document.createElement('button');
-                this.suivant.innerHTML = 'suivant';
-                this.buttonContainer.appendChild(this.suivant);
-                this.suivant.addEventListener('click', () => this.getUrl10());
+               
+                this.numberPhoto += 10;
             }
             catch(e){
                 console.error(`Une erreur s'est produite : ${e.message}`)
-                this.hideLoadingImage();
+              //  this.hideLoadingImage();
             }
-        }
-
-        async getUrl20(){
-            try{
-                for (let i =(this.numberPhoto+10); i<(20+this.numberPhoto) ; i++){
-                    const id = i
-                    let responseImg = await fetch (`https://jsonplaceholder.typicode.com/photos?id=${id}`)
-                    let imgUrl =  await responseImg.json()
-                    let url = imgUrl[0]?.url;
-                    const imageElement = document.createElement('img');
-                    imageElement.src = url;
-                    this.container.appendChild(imageElement);
-
-
-                }
-                this.buttonContainer = document.createElement('div');
-                this.areaDom.appendChild(this.buttonContainer);
-            
-                this.suivant = document.createElement('button');
-                this.suivant.innerHTML = 'suivant';
-                this.buttonContainer.appendChild(this.suivant);
-                this.suivant.addEventListener('click', () => this.getUrl10());
-            }
-            catch(e){
-                console.error(`Une erreur s'est produite : ${e.message}`)
-            }
-        }
-   
-    
+        } 
     
 }
     
