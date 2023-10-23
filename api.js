89689598d35fb843c814bc72a0ced0ed2b9b5ef1
Marion.girard <marion.girard''@gmail.com>
2023-10-23 14:21:44 +0200

    le code marche

--- api.js
+++ api.js
@@ -14,6 +14,7 @@ class LoadPicture{
 
         this.createInterface()
         this.parcourir()
+        
 
 
 
@@ -26,12 +27,15 @@ class LoadPicture{
         if (!this.areaDom)
         throw new Error(`SLIDER : la zone du DOM "${this.area}" n'existe pas dans le document HTML`)
 
+        this.container = document.createElement('div');
+        this.areaDom.appendChild(this.container);
 
         this.suivant = document.createElement('button');
         this.suivant.innerHTML = 'suivant';
-        this.suivant.addEventListener('click', ()=>{this.parcourirNuberPhoto()}); // exemple avec fonction fléchée (pas de changement de contexte) + appel de next
-
-        this.areaDom.appendChild(this.suivant);
+        this.container.appendChild(this.suivant);
+        this.suivant.addEventListener('click',() => this.parcourir10()); // exemple avec fonction fléchée (pas de changement de contexte) + appel de next
+        
+    
     }
     parcourir(){
         
@@ -53,9 +57,9 @@ class LoadPicture{
             
                 const imageElement = document.createElement('img');
                 imageElement.src = imageUrl;
-                ;
                 
-                this.areaDom.appendChild(imageElement);
+                
+                this.container.appendChild(imageElement);
                 
                 // Afficher l'URL de l'image dans la console et sur la page
                 /*console.log("L'URL de l'image est : ", imageUrl);
@@ -69,8 +73,8 @@ class LoadPicture{
         }
     }
 
-    parcourirNuberPhoto(){
-        
+    /*parcourirNuberPhoto(){
+        console.log("10 de plus")
         for (let i = this.numberPhoto; i<this.numberPhoto ; i++){
             const id = i
             
@@ -89,22 +93,57 @@ class LoadPicture{
             
                 const imageElement = document.createElement('img');
                 imageElement.src = imageUrl;
-                ;
+                
                 
                 this.areaDom.appendChild(imageElement);
-                
+                */
                 // Afficher l'URL de l'image dans la console et sur la page
                 /*console.log("L'URL de l'image est : ", imageUrl);
                 const urlElement = document.createElement('p');
                 urlElement.textContent = `L'URL de l'image est : ${imageUrl}`;
                 document.body.appendChild(urlElement);*/ // Ajoute l'élément au corps du document
+               /* })
+                .catch(error => console.error('Une erreur s\'est produite lors de la récupération des données de la photo', error));
+        
+                
+            }
+        }*/
+    parcourir10(){
+        for (let i =10; i<20 ; i++){
+            const id = i
+            
+            const url = `https://jsonplaceholder.typicode.com/photos?id=${id}`;
+            
+            // Récupérer les données de la photo
+            fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erreur de réseau ou de réponse non valide');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const imageUrl = data[0].url; // Récupérer l'URL de l'image à partir des données
+            
+                const imageElement = document.createElement('img');
+                imageElement.src = imageUrl;
+                this.container.appendChild(imageElement);
+                /*const div = document.createElement("div")
+                document.body.appendChild(div)
+                div.appendChild(imageElement);*/
+                
+                // Afficher l'URL de l'image dans la console et sur la page
+                /*console.log("L'URL de l'image est : ", imageUrl);
+                const urlElement = document.createElement('p');
+                urlElement.textContent = `L'URL de l'image est : ${imageUrl}`;
+                document.body.appendChild(urlElement); // Ajoute l'élément au corps du document*/
                 })
                 .catch(error => console.error('Une erreur s\'est produite lors de la récupération des données de la photo', error));
         
                 
         }
-    }
 }
+    }
     
     
         
@@ -116,43 +155,8 @@ class LoadPicture{
         
         // Ajouter le bouton à la page
         document.body.appendChild(suivant);
-        
-        function parcourir10(){
-            for (let i =10; i<20 ; i++){
-                const id = i
-                
-                const url = `https://jsonplaceholder.typicode.com/photos?id=${id}`;
-                
-                // Récupérer les données de la photo
-                fetch(url)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Erreur de réseau ou de réponse non valide');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    const imageUrl = data[0].url; // Récupérer l'URL de l'image à partir des données
-                
-                    const imageElement = document.createElement('img');
-                    imageElement.src = imageUrl;
-                    ;
-                    const div = document.createElement("div")
-                    document.body.appendChild(div)
-                    div.appendChild(imageElement);
-                    
-                    // Afficher l'URL de l'image dans la console et sur la page
-                    console.log("L'URL de l'image est : ", imageUrl);
-                    const urlElement = document.createElement('p');
-                    urlElement.textContent = `L'URL de l'image est : ${imageUrl}`;
-                    document.body.appendChild(urlElement); // Ajoute l'élément au corps du document
-                    })
-                    .catch(error => console.error('Une erreur s\'est produite lors de la récupération des données de la photo', error));
-            
-                    
-            }
-        }
-
+        */
+/*
         async function getUrl(){
             try{
                 for (let i =0; i<10 ; i++){
@@ -170,6 +174,7 @@ class LoadPicture{
         const pictures = new LoadPicture({
             url: 'https://jsonplaceholder.typicode.com/photos',
             numberPhoto: 10,
+            
             
 
         });
